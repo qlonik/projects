722fc1ea58babff385ed8f88c9a1a799913943bb
Nikita Volodin <volodin.n@gmail.com>
2022-06-29 23:01:48 -0400

    fix: exclude inspect from regular log method
    This way messages printed by the script to console do not have weird
    line breaks within them.

---
+++ .changeset/tasty-bees-clean.md
@@ -0,0 +1,8 @@
+---
+"@qlonik/semantic-commit-to-changeset": patch
+---
+
+exclude inspect from regular log method
+
+This way messages printed by the script to console do not have weird
+line breaks within them.
--- packages/semantic-commit-to-changeset/src/impl/log.ts
+++ packages/semantic-commit-to-changeset/src/impl/log.ts
@@ -1,7 +1,10 @@
 import * as T from "@effect-ts/core/Effect"
-import { inspect } from "util"
+import { inspect as inspect_ } from "util"
+
+export const inspect = <A extends readonly unknown[]>(...args: A) =>
+  T.succeedWith(() =>
+    console.log(...args.map((a) => inspect_(a, { depth: null }))),
+  )
 
 export const log = <A extends readonly unknown[]>(...args: A) =>
-  T.succeedWith(() =>
-    console.log(...args.map((a) => inspect(a, { depth: null }))),
-  )
+  T.succeedWith(() => console.log(...args))
